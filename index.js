e1e4df6ff2f075ad1290f2e02281dc98489c118f
cdsalty <soltiscd@gmail.com>
2022-12-08 21:37:02 -0500

    need to check error and then display results

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Middleware
 // Enable body parser
 app.use(express.json());
 // this is allow us to accept body data that comes in from request.body
 app.use(express.urlencoded({ extended: false }));
+
+// Setup middleware/static folder to route to our public page
+app.use(express.static(path.join(__dirname, "public")));
 
 // for the route /openai, we want to require the openaiRoutes
 app.use("/openai", require("./routes/openaiRoutes"));
---
+++ public/js/aigen.js
@@ -0,0 +1,53 @@
+function onSubmit(e) {
+  e.preventDefault();
+
+  const prompt = document.querySelector("#prompt").value;
+  const size = document.querySelector("#size").value;
+
+  if (prompt === "") {
+    alert("Please use some kind of an imigination");
+  }
+
+  // test out functionality before continuing. Once it works, we need to create an async function to handle the request and generate the image
+  // console.log("Submit properly works");
+
+  requestAiImage(prompt, size);
+}
+
+// 3) create the async function to requestAiImage
+async function requestAiImage(prompt, size) {
+  try {
+    displaySpinner();
+    const response = await fetch("/openai/generateimage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("That image could not be generated.");
+    }
+    const data = await response.json();
+    console.log(data);
+    removeSpinner();
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
+}
+
+// 4) Create functions to show or hide a spinner
+function displaySpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function removeSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
+// 1) Start off by adding event listener to the form
+document.querySelector("#image-form").addEventListener("submit", onSubmit);
